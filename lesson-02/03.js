79b8de954126a74219e113340f9c7c153d765c65
DenisEremenkoDev <Deniseremenkodev@gmail.com>
2024-08-30 02:15:44 +0300

    complete task

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -12,18 +12,18 @@ let grade
 
 if (score > 0 && score <= 49) {
     grade = "F"
-    }
-        else {
-            if (score >= 50 && score <= 69) {
-            grade = "D"
-            } if (score >= 70 && score <= 79) {
+    } if (score >= 50 && score <= 69) {
+        grade = "D"
+        } if (score >= 70 && score <= 79) {
             grade = "C"
             } if (score >= 80 && score <= 89) {
-            grade = "B"
-            } if (score >= 90 && score <= 100) {
-            grade = "A"
-            }
-        }
+                    grade = "B"
+                    } if (score >= 90 && score <= 100) {
+                            grade = "A"
+                            } 
+                            else {
+                                console.log("incorrect input");
+                                }
 console.log(grade);
 
 // "F" = (0 - 49)

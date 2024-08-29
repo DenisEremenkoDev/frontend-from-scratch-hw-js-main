5a73642eb202485c1d0de342e612178eb78f5272
DenisEremenkoDev <Deniseremenkodev@gmail.com>
2024-08-30 00:05:22 +0300

    complete task 2-3

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -7,3 +7,26 @@
 const score = 70 // тестовое значение, можно изменять
 let grade
 // your code
+
+if (score > 0 && score <= 49) {
+    grade = "F"
+    console.log(grade);
+    } if (score >= 50 && score <= 69) {
+        grade = "D"
+        console.log(grade);
+        } if (score >= 70 && score <= 79) {
+            grade = "C"
+            console.log(grade);
+                } if (score >= 80 && score <= 89) {
+                    grade = "B"
+                    console.log(grade);
+                        } if (score >= 90 && score <= 100) {
+                            grade = "A"
+                            console.log(grade);
+} 
+
+// "F" = (0 - 49)
+// "D" = (50 - 69)
+// "C" = (70 - 79)
+// "B" = (80 - 89)
+// "A" = (90 - 100 )

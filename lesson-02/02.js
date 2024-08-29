cb10c96672c41e2ee9ba0bc4d5b97a8a253d97bc
DenisEremenkoDev <Deniseremenkodev@gmail.com>
2024-08-29 23:13:29 +0300

    complete task-2-2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -9,15 +9,15 @@
 
 let dice1 = Math.floor(Math.random() * 7)
 let dice2 = Math.floor(Math.random() * 7)
-let isWinningDouble = false
+let isWinningDouble
 
 
 
 
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
-if (isWinningDouble > 3 || dice1 === dice2) {
-  let isWinningDouble = true
+
+if (isWinningDouble = dice1 === dice1 && dice2 > 3) {
   console.log('Выигрышный дубль!')
 } else {
   console.log('Не выигрышный дубль.')

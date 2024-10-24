466134347d4b5602f7cd1d0e1f987d94166a0a88
DenisEremenkoDev <Deniseremenkodev@gmail.com>
2024-10-24 18:43:41 +0300

    complete task 3-2.1

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -2,15 +2,18 @@
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
 function isEven(number) {
-    if (number%2 == 1) {
-        // let result = false
-        // return result
-        return false
-    } else {
-        // let result = true
-        // return result
+    if (number%2 === 0) {
         return true
+    } else {
+        return false
     }
 }
-const resultNumber = isEven(2)
-// console.log(resultNumber);
+const resultNumber = isEven(-1)
+console.log(resultNumber);
+
+// function isEven(number) {
+//     return number % 2 === 0 ? true : false
+// }
+
+// console.log(isEven(-1));
+// console.log(isEven(4));
